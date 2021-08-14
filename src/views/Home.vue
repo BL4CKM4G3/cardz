00301806c596d3e4885981d335350d8eec12e36e
BL4CKM4G3 <alvaro_rangel@outlook.com>
2021-08-14 14:26:56 -0300

    feat: :sparkles: Creating and improving Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,64 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <sidebar></sidebar>
+    <card v-for="card in cards" :key="card.id" :content="card"></card>
   </div>
 </template>
 
 <script>
+import Card from '../components/Card.vue'
+import Sidebar from '../components/Sidebar.vue'
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
+    Sidebar,
+    Card
+  },
+  data: () => ({
+    cards: [
+      {
+        id: 1,
+        title: 'Create a design for Acme Corporation',
+        level: 'medium',
+        subtitle: 'need to focus',
+        priority: 'high',
+        description: 'Need to create a design for Acme Corporation, need to think as a child watching a cartoon.'
+      },
+      {
+        id: 2,
+        title: 'Create a design for Acme Corporation',
+        level: 'medium',
+        subtitle: 'need to focus',
+        priority: 'high',
+        description: 'Need to create a design for Acme Corporation, need to think as a child watching a cartoon.'
+      },
+      {
+        id: 3,
+        title: 'Create a design for Acme Corporation',
+        level: 'medium',
+        subtitle: 'need to focus',
+        priority: 'high',
+        description: 'Need to create a design for Acme Corporation, need to think as a child watching a cartoon.'
+      },
+      {
+        id: 4,
+        title: 'Create a design for Acme Corporation',
+        level: 'medium',
+        subtitle: 'need to focus',
+        priority: 'high',
+        description: 'Need to create a design for Acme Corporation, need to think as a child watching a cartoon.'
+      },
+    ]
+  })
 }
 </script>
+
+<style lang="scss" scoped>
+  .home {
+    display: flex;
+    flex-direction: row;
+    width: auto;
+  }
+</style>
