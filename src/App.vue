44ca546d15315a74fc5827a7a5359c3a67215196
BL4CKM4G3 <alvaro_rangel@outlook.com>
2021-08-14 14:27:20 -0300

    feat: :sparkles: Improving App main component

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,26 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <h1>cardz</h1>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  @import "./assets/stylesheets/lib/index.scss";
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+  #nav {
+    h1 {
+      transition: all .05s ease-out;
+      &:hover {
+        font-weight: 500;
+        color: var(--secondary-hover);
+        transition: all .15s ease-in;
+      }
     }
+    margin: 0 auto 0 auto;
+    width: 20vw;
+    border-bottom: 1px solid var(--caption);
   }
-}
 </style>
